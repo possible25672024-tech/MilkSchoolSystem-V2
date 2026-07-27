2e7475880663b551f90c296e3f7fe956f0df2d1a
possible25672024-tech <possible25672024@gmail.com>
2026-07-27 17:34:05 +0700

    refactor(core): start application through login manager

--- modules/core/app.js
+++ modules/core/app.js
@@ -1,12 +1,23 @@
-class MilkSchoolApplication{
-
-    async start(){
-
-        console.log("MilkSchoolSystem V2 Started");
-
+class MilkSchoolApplication {
+    constructor(loginManager = window.LoginManager) {
+        this.loginManager = loginManager;
+        this.started = false;
     }
 
+    async start() {
+        if (this.started) {
+            return;
+        }
+
+        if (!this.loginManager) {
+            throw new Error("LoginManager is not available.");
+        }
+
+        await this.loginManager.initialize();
+        this.started = true;
+
+        console.log("MilkSchoolSystem V2 Started");
+    }
 }
 
-window.App =
-new MilkSchoolApplication();
+window.App = new MilkSchoolApplication();
