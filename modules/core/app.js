1d7cd2634358aa75cc1d55a786a3dfacdbb64e5c
possible25672024-tech <possible25672024@gmail.com>
2026-07-28 20:36:29 +0700

    feat(app): initialize Offline Queue view after Attendance UI

--- modules/core/app.js
+++ modules/core/app.js
@@ -2,12 +2,23 @@ class MilkSchoolApplication {
     constructor(
         loginManager = window.LoginManager,
         teacherView = window.TeacherView,
-        attendanceView = window.AttendanceView
+        attendanceView = window.AttendanceView,
+        syncView = window.SyncView
     ) {
         this.loginManager = loginManager;
         this.teacherView = teacherView;
         this.attendanceView = attendanceView;
+        this.syncView = syncView;
         this.started = false;
+    }
+
+    async ensureSyncView() {
+        if (!this.syncView) {
+            await import("../sync/syncView.js");
+            this.syncView = window.SyncView;
+        }
+
+        return this.syncView;
     }
 
     async start() {
@@ -27,6 +38,11 @@ class MilkSchoolApplication {
 
         if (this.attendanceView?.initialize) {
             await this.attendanceView.initialize();
+        }
+
+        const syncView = await this.ensureSyncView();
+        if (syncView?.initialize) {
+            await syncView.initialize();
         }
 
         this.started = true;
